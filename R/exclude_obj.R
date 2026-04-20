b0ec0617239f4a6b936caa4f4527109a8ed02466
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2026-04-20 21:27:21 +0100

    Update documentation

--- R/exclude_obj.R
+++ R/exclude_obj.R
@@ -1,12 +1,15 @@
 
 #' Helper to exclude key combinations from a transformation
+#' @param ... list specifying key combinations to exclude
 exclude <- function(...) {
 
   structure(, class = "exclude")
 }
 
+#' Print exclude object
+#' @param x exclude object
+#' @param ... pass to method
 print.exclude <- function(x, ...) {
 
 }
 
-new_exclude
